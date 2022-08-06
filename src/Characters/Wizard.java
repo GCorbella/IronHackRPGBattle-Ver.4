925761b8b66b60dfa8f442bef9800a672ed1beae
RaulGalanDelgado <110462806+RaulGalanDelgado@users.noreply.github.com>
2022-08-06 17:56:57 +0200

    classes ok

--- src/Characters/Wizard.java
+++ src/Characters/Wizard.java
@@ -1,2 +1,62 @@
-package Characters;public class Wizard {
+package PACKAGE_NAME;
+public class Wizard extends Character {
+
+
+    private int mana;
+    private int intelligence;
+
+
+    public Wizard(String name, int hp, int mana, int intelligence) {
+        super(name, hp);
+        this.mana = mana;
+        this.intelligence = intelligence;
+        setClassName("Wizard");
+    }
+    // method attack.
+
+    public int[] attack () {
+
+        int[] damage = new int[2];
+
+        //fuerte
+        if( getMana() >= 5 ) {
+            damage[0] = getIntelligence();
+            setMana(getMana() + 1);
+            damage[1] = 1;
+        }
+        //debil
+        else {
+            damage[0] = 2;
+            setMana(getMana() + 1);
+            damage[1] = 2;
+        }
+
+        return damage;
+    }
+
+    //Getters and setters
+
+    public int getMana() {
+        return mana;
+    }
+
+    public void setMana(int mana) {
+        this.mana = mana;
+    }
+
+    public int getIntelligence() {
+        return intelligence;
+    }
+
+    public void setIntelligence(int intelligence) {
+        this.intelligence = intelligence;
+    }
+
+    public String toString(){
+        return "Name: " +getName()  + " Type: Wizard HP: " +
+                getHp() + " Intelligence: " + getIntelligence() +" Mana: " + getMana() + "\n";
+    }
+}
+
+
 }
