8243c19b202bc52a64151c8bb44dda6b73397b00
MarcMarimon <crammarimon@hotmail.com>
2022-08-09 17:32:44 +0200

    Arrancamos

--- src/Characters/Character.java
+++ src/Characters/Character.java
@@ -1,2 +1,3 @@
 package Characters;public class Character {
-}
+
+ }
--- src/Teams/Teams.java
+++ src/Teams/Teams.java
@@ -1,4 +1,34 @@
 package Teams;
+import Characters.Character;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
 public class Teams {
+
+public static Character[] createNewTeam(){
+
+    Character[] newTeam = new Character[5];
+    for (int i=0; i< newTeam().length;i++){
+        newTeam[i]= new Character;
+
+        return createNewTeam(newTeam);
+    }
+
+public static  Character[] importATeam(){
+        File team = new File("team.csv");
+        Character [] importedTeam = new Character[5];
+        try{
+            Scanner inputFile = new Scanner(team);
+            inputFile.useDelimiter(",");
+            while (inputFile.hasNext()){
+
+            }
+            inputFile.close();
+        }catch (FileNotFoundException e){
+            System.out.println("Check file");
+        }
+    }
+  }
+
 }
